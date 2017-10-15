cd5a138bf9ba25138bf6792fb750c3ea3adf262a
Matt Divertie <mdivertie@gmail.com>
2017-10-15 14:37:09 -0500

    import statements
    some imports

--- JavaChess/src/edu/uwrf/segroup/ChessGUI.java
+++ JavaChess/src/edu/uwrf/segroup/ChessGUI.java
@@ -1,9 +1,12 @@
 package edu.uwrf.segroup;
 
+import java.awt.event.*;
 import javax.swing.*;
 
-public class ChessGUI {
+public class ChessGUI  extends JFrame implements ActionListener{
 	
 	// This will be the GUI Does this even work?
+	
+	
 
 }
