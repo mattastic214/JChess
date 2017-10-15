0f8c70539ed8b91f5ef0ca76df715e0900cd2cb7
Matt Divertie <mdivertie@gmail.com>
2017-10-15 15:45:27 -0500

    Add chessboard
    Add Chessboard... Kind of

--- JavaChess/src/edu/uwrf/segroup/ChessGUI.java
+++ JavaChess/src/edu/uwrf/segroup/ChessGUI.java
@@ -1,11 +1,29 @@
 package edu.uwrf.segroup;
 
+import java.awt.BorderLayout;
 import java.awt.event.*;
-import javax.swing.*;;
+import java.net.URL;
+
+import javax.swing.*;
 
 public class ChessGUI  extends JFrame implements ActionListener{
 
+	private static final int HEIGHT = 700;
+	private static final int WIDTH = 600;
 	
+	private URL url = ChessGUI.class.getResource("/resources/chessboard.png");
+	
+	/**
+	 * The null constructor for the GUI
+	 */
+	public ChessGUI() {
+		
+		super("Chess Lounge");
+		setVisible(true);
+		setSize(HEIGHT, WIDTH);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+	}
 	
 	
 	@Override
