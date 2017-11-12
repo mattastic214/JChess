adf703750ff79fa9d0158d8f9a7971b653699e98
Matt Divertie <mdivertie@gmail.com>
2017-11-12 17:13:37 -0600

    updates and comments

--- JavaChess/src/edu/uwrf/segroup/model/ChessBoard.java
+++ JavaChess/src/edu/uwrf/segroup/model/ChessBoard.java
@@ -2,6 +2,18 @@ package edu.uwrf.segroup.model;
 
 import java.util.ArrayList;
 
+/**
+ * ChessBoard is a class that initiates an 
+ * instance of a chess board. 
+ * <p>
+ * The class is 
+ * responsible for populating the chess board
+ * with Squares, white and black ChessPieces,
+ * and finally populating the Squares with the
+ * appropriate pieces. 
+ * @author matta
+ *
+ */
 public class ChessBoard {
 
 	private Square[][] squares;
@@ -9,6 +21,10 @@ public class ChessBoard {
 	private ArrayList<ChessPiece> blackPieces;
 	
 	public ChessBoard(IBoardFactory boardFactory) {
-		// TODO Auto-generated constructor stub
+		
+		this.squares = boardFactory.createSquares();
+		this.whitePieces = boardFactory.createPieces(Side.WHITE);
+		this.blackPieces = boardFactory.createPieces(Side.BLACK);
+		boardFactory.populateSquares(this.squares);
 	}
 }
--- JavaChess/src/edu/uwrf/segroup/model/Square.java
+++ JavaChess/src/edu/uwrf/segroup/model/Square.java
@@ -1,23 +1,70 @@
 package edu.uwrf.segroup.model;
 
+/**
+ * Square is a class that represents a square on a chess board. Square
+ * contains information about its location on the board. Square also has 
+ * a reference to a {@link ChessPiece} that occupies it.
+ * <p>
+ * Square checks for collisions that occur if one ChessPiece moves to Square
+ * that is already occupied.
+ * @author matta
+ *
+ */
 public class Square {
 
+	private int rowID;
+	private int colID;
 	private ChessPiece occupier;
 	
-	public Square() {
+	/**
+	 * Initializes an instance of a Square object
+	 * @param row
+	 * 				The Square object's row
+	 * @param col
+	 * 				The Square object's column
+	 */
+	public Square(int row, int col) {
+		rowID = row;
+		colID = col;
 		occupier = null;
 	}
+	
+	
+	public int getRowID() {
+		return rowID;
+	}
+	
+	public int getColID() {
+		return colID;
+	}
+	
 	
 	public ChessPiece getOccupier() {
 		return occupier;
 	}
 	
+	/**
+	 * When a
+	 * @param entrant
+	 */
 	public void setOccupier(ChessPiece entrant) {
-		this.occupier = entrant;
+		
+		if(this.checkCollision())
+			;// ToDo: throw an event to reset the piece to its original position
+		else
+			this.occupier = entrant;
 	}
 	
 	public void vacate() {
 		occupier = null;
+	}
+	
+	private boolean checkCollision() {
+		
+		if(occupier == null)
+			return false;
+		else 
+			return true;
 	}
 	
 }
