8736ec57b1e44878ff8e94a33c01375fb8a08ae0
Matt Divertie <mdivertie@gmail.com>
2017-11-26 20:08:02 -0600

    comments
    more comments

--- JavaChess/src/edu/uwrf/segroup/model/ChessPiece.java
+++ JavaChess/src/edu/uwrf/segroup/model/ChessPiece.java
@@ -4,13 +4,23 @@ import java.net.URL;
 
 import javax.swing.ImageIcon;
 
+/**
+ * The abstract class ChessPiece represents chess pieces in the game of chess. ChessPiece
+ * has fields and methods that are common to all extensions of ChessPiece
+ * @author matta
+ *
+ */
 public abstract class ChessPiece {
 	
 	protected Side team;
 	protected ImageIcon image;
 	
+	/**
+	 * A constructor for a ChessPiece.
+	 * @param setTeam Sets the ChessPiece team to either WHITE or BLACK enum type
+	 * @param imagePath The path of the image that is used to represent the ChessPiece.
+	 */
 	public ChessPiece(Side setTeam, String imagePath) {
-		
 		
 		URL iStream = getClass().getResource(imagePath);
 		image =  new ImageIcon(iStream);
@@ -19,36 +29,30 @@ public abstract class ChessPiece {
 		
 	}
 	
+	/**
+	 * 
+	 * @return Gets the team enum type of the ChessPiece. Can be used
+	 * to inform us what color or which side a ChessPiece is on.
+	 */
 	public Side getTeam() {
 		return team;
 	}
 	
+	/**
+	 * 
+	 * @return Gets the ImageIcon of the ChessPiece.
+	 */
 	public ImageIcon getImage() {
 		
 		return image;
 	}
 	
-	
-	/*
-	public void move(Square dest) {
-		
-		if(!checkFriendlyCollision(dest))
-			enforceRules(dest);
-	}*/
-	
+	/**
+	 * This abstract method specifies the rules a ChessPiece should follow when moving
+	 * from one Square to another.
+	 * @param origin The Square from which a ChessPiece is moving.
+	 * @param dest The Square to which a ChessPiece is moving.
+	 */
 	protected abstract void moveRules(Square origin, Square dest);
 	
-	/**
-	// Needs to check Side of piece in Square
-	protected boolean checkFriendlyCollision(Square dest) {
-		
-		if(dest.getOccupier().getTeam() == this.getTeam())
-			return true;
-		else if(dest.getOccupier() != null) {
-			dest.setOccupier(null);
-			return false;
-		}
-		return false;
-	}
-	*/
 }
