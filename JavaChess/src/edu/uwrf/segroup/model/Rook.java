a5e5bd6f625b3c6ad6dc968eaef246806a234ff4
Matt Divertie <mdivertie@gmail.com>
2017-11-28 08:44:21 -0600

    Rook Rules?
    Hey, tried to make rook rules. Makes it to the end of the method, but the images don't update.

--- JavaChess/src/edu/uwrf/segroup/model/Rook.java
+++ JavaChess/src/edu/uwrf/segroup/model/Rook.java
@@ -1,5 +1,11 @@
 package edu.uwrf.segroup.model;
 
+
+/**
+ * An extension of ChessPiece
+ * @author matta
+ *
+ */
 public class Rook extends ChessPiece {
 
 	public Rook(Side setTeam, String imagePath) {
@@ -8,9 +14,210 @@ public class Rook extends ChessPiece {
 	}
 
 	@Override
-
 	public void moveRules(Square origin, Square[][] chessboard, int random) {
-		// TODO Auto-generated method stub
+
+		if(origin.getFlag() == true) {
+			
+			for(int row = 0; row < Settings.NUM_ROWS; row++){
+				
+				for(int col = 0; col <Settings.NUM_COLS; col++) {
+					// Check which Square was clicked and set it's flag to true
+					if(chessboard[row][col].getFlag() == true && chessboard[row][col] != origin) {
+						
+						if (random == 0) {
+							
+							if(team == Side.BLACK) {
+								
+								// Moving up or down
+								if(chessboard[row][col].getColID() == origin.getColID()) {
+									// Moving up
+									if (row > origin.getRowID()) {
+										for(int i = origin.getRowID() + 1; i < row; i++) {
+											if(chessboard[i][col].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+										
+									}
+									// Moving down
+									if(row < origin.getRowID()) {
+										for(int i = origin.getRowID() - 1; i > row; i--) {
+											if(chessboard[i][col].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+										
+									}
+								
+								}
+								
+								// Moving side to side
+								else if(chessboard[row][col].getRowID() == origin.getRowID()) {
+									// Moving Left
+									if (col < origin.getColID()) {
+										for (int i = origin.getColID() - 1; i > col; i--) {
+											if(chessboard[row][i].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+									// Moving Right
+									if (col > origin.getColID()) {
+										for(int i = origin.getColID() + 1; i < col; i++) {
+											if(chessboard[row][i].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+								}
+							}
+							else if(team == Side.WHITE) {
+								// Moving up or down
+								if(chessboard[row][col].getColID() == origin.getColID()) {
+									// Moving up
+									if (row > origin.getRowID()) {
+										for(int i = origin.getRowID() + 1; i < row; i++) {
+											if(chessboard[i][col].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+									// Moving down
+									if(row < origin.getRowID()) {
+										for(int i = origin.getRowID() - 1; i > row; i--) {
+											if(chessboard[i][col].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+								}
+								// Moving side to side
+								else if(chessboard[row][col].getRowID() == origin.getRowID()) {
+									// Moving Left
+									if (col < origin.getColID()) {
+										for (int i = origin.getColID() - 1; i > col; i--) {
+											if(chessboard[row][i].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+									// Moving Right
+									if (col > origin.getColID()) {
+										for(int i = origin.getColID() + 1; i < col; i++) {
+											if(chessboard[row][i].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+								}
+							}
+						}
+						else if (random == 1) {
+							
+							if(team == Side.BLACK) {
+								
+								// Moving up or down
+								if(chessboard[row][col].getColID() == origin.getColID()) {
+									// Moving up
+									if (row > origin.getRowID()) {
+										for(int i = origin.getRowID() + 1; i < row; i++) {
+											if(chessboard[i][col].getOccupier() != null) ; 
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+									// Moving down
+									if(row < origin.getRowID()) {
+										for(int i = origin.getRowID() - 1; i > row; i--) {
+											if(chessboard[i][col].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+								}
+								// Moving side to side
+								else if(chessboard[row][col].getRowID() == origin.getRowID()) {
+									// Moving Left
+									if (col < origin.getColID()) {
+										for (int i = origin.getColID() - 1; i > col; i--) {
+											if(chessboard[row][i].getOccupier() != null) ; 
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+									// Moving Right
+									if (col > origin.getColID()) {
+										for(int i = origin.getColID() + 1; i < col; i++) {
+											if(chessboard[row][i].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+								}
+							}
+							else if(team == Side.WHITE) {
+								// Moving up or down
+								if(chessboard[row][col].getColID() == origin.getColID()) {
+									// Moving up
+									if (row > origin.getRowID()) {
+										for(int i = origin.getRowID() + 1; i < row; i++) {
+											if(chessboard[i][col].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+									// Moving down
+									if(row < origin.getRowID()) {
+										for(int i = origin.getRowID() - 1; i > row; i--) {
+											if(chessboard[i][col].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+								}
+								// Moving side to side
+								else if(chessboard[row][col].getRowID() == origin.getRowID()) {
+									// Moving Left
+									if (col < origin.getColID()) {
+										for (int i = origin.getColID() - 1; i > col; i--) {
+											if(chessboard[row][i].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+									// Moving Right
+									if (col > origin.getColID()) {
+										for(int i = origin.getColID() + 1; i < col; i++) {
+											if(chessboard[row][i].getOccupier() != null) ;
+												//return;
+										}
+										chessboard[row][col].setOccupier(origin.getOccupier());
+										origin.vacate();
+									}
+								}
+							}
+						}
+					}
+				}
+				System.out.println("It Works!!");
+				break;
+			}
+		}
 		
 	}
 
